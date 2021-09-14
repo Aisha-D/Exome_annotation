e5b7510ad6bb8b52398accf43be0186962dafa5b
Aisha-D <AishaDahir75@gmail.com>
2021-09-14 18:55:42 +0100

    Loop through transcripts file

--- cellbase/cellbase5_exons.py
+++ cellbase/cellbase5_exons.py
@@ -3,50 +3,70 @@ from pycellbase.cbconfig import ConfigClient
 from pycellbase.cbclient import CellBaseClient
 import pandas as pd
 from pandas.core.frame import DataFrame
+import urllib.request, json
 
 from host import host_address
 
-## Query cellbase5 database 
-custom_config = {'rest': {'hosts': [host_address]}, 'version': 'v5', 'species': 'hsapiens'}
-cc = ConfigClient(custom_config)
-cbc = CellBaseClient(cc)
-cbc.show_configuration()['version']
-gc = cbc.get_gene_client() #select gene clients
 
-# Read in the HGNC list that currently exists 
-HGNC_df = pd.read_csv('HGNC_210902.tsv', sep='\t')
-# this list will contain all the HGNC_ids and relevant MANE RefSeq transcripts
-all_genes = []
+## 14/09/21
+def flip_exonNumber(transcript_table):
+    print("Negative strand! Flipping exons.")
+    exonNumber = list(transcript_table.exonNumber)
+    transcript_table2 = transcript_table.drop(columns=['exonNumber'])
+    exonNumber.reverse()
+    en = pd.DataFrame({"exonNumber" : exonNumber})
+    transcript_table2 = pd.concat([transcript_table2, en], axis = 1)
+    return(transcript_table2)
 
-HGNC_id = HGNC_df.iloc[row, 0] #HGNC ID is in 0th column
-ensemble_id = HGNC_df.iloc[row, 11] #Ensembl gene ID is in 12th column
-# some genes do not have ensemble id so skip these
-if pd.isna(ensemble_id):
-    print(HGNC_id + " does not have ensemble id to gene")
-    pass
-else:
-    # make dict to keep info on what refseq is for ensemble gene in cellbase
-    gene_dict = {}
-    gene_dict['HGNC_ID'] = HGNC_id
-    # get all info and select transcript info
-    data = gc.get_info(ensemble_id)
-    # some ensemble gene id not present in cellbase so skip these
-    if not data['responses'][0]['results']:
-        print(ensemble_id + " does not exist in cellbase5") 
-    else:
-        # how many transcripts are there?
-        txs_num = len(data['responses'][0]['results'][000]['transcripts'])
-        # for loop the ensemble transcripts to find what the refseq mane 
-        # transcripts are for the ensemble gene
-        for transcript in range(txs_num):
-            dicts = list(data['responses'][0]['results'][0]['transcripts'][transcript]['xrefs'])
-            mane_dict = [item for item in dicts if item["dbName"] == "mane_select_refseq"]
-            if not mane_dict:
-                # some ensemble transcripts do not have refseq mane transcript
-                # for these, we skip to the next transcript
-                continue
-            else:
-                # refseq mane id is selected and added to the dict
-                gene_dict['MANE_RefSeqID'] = mane_dict[0]['id']
-        # append the gene_dict to the list of all HGNC ids
-        all_genes.append(gene_dict) 
+
+def query_cellbasedict(exon_dict):
+    txs_dict = {}
+    txs_dict['chr'] = exon_dict["chromosome"]
+    txs_dict['exon_start'] = exon_dict["start"]
+    txs_dict['exon_end'] = exon_dict["end"]
+    txs_dict['gene_symbol'] = data['responses'][0]['results'][0]['name']
+    txs_dict['transcript_id'] = data['responses'][0]['results'][0]['id'] #cannot take from exon as it has the _exonnumber attached to the transcript
+    txs_dict['exonNumber'] = exon_dict["exonNumber"]
+    #print(txs_dict)
+    return txs_dict
+
+# contains all transcripts
+df = pd.DataFrame() 
+
+g2t = pd.read_csv('g2t_210907_b38.tsv', sep='\t')
+
+mane_transcripts = list(g2t.iloc[:,1])
+len(mane_transcripts)
+for transcript in range(10):
+    print(mane_transcripts[transcript])
+    url_get = "/webservices/rest/v5/hsapiens/feature/transcript/"
+    txs = mane_transcripts[transcript]
+    url_end = "/info?source=refseq"
+    url_address = host_address + url_get + txs + url_end
+    with urllib.request.urlopen(url_address) as url:
+        data = json.loads(url.read().decode())
+    
+    #number of exons: 
+    exons_num = len(data['responses'][0]['results'][0]['exons'])
+    
+    list_txs_dict = []
+    for exon in range(exons_num):
+        # print(exon)
+        exon_dict = data['responses'][0]['results'][0]['exons'][exon]
+        #print(exon_dict)
+        if exon_dict["phase"] != -1:
+            extracted_exons_dict = query_cellbasedict(exon_dict)
+            list_txs_dict.append(extracted_exons_dict)
+            # print(list_txs_dict)
+        else:
+            print("exon " + str(exon) + " is a non coding exon")
+    
+    # combine all exons into one table for that transcript
+    transcript_table = pd.DataFrame(list_txs_dict)
+    
+    if data['responses'][0]['results'][0]['strand'] == "-":
+        transcript_table2 = flip_exonNumber(transcript_table)
+    
+    df = df.append(transcript_table2) 
+
+df.to_csv("exon_test.tsv", sep="\t", header=True, index=False)
