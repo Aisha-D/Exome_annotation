996a2375fd703a3382fb6f6e506a0ed11556eae7
Aisha-D <AishaDahir75@gmail.com>
2021-09-15 14:41:14 +0100

    include pos strand transcripts

--- cellbase/cellbase5_exons.py
+++ cellbase/cellbase5_exons.py
@@ -10,7 +10,7 @@ from host import host_address
 
 ## 14/09/21
 def flip_exonNumber(transcript_table):
-    print("Negative strand! Flipping exons.")
+    # print("Negative strand! Flipping exons.")
     exonNumber = list(transcript_table.exonNumber)
     transcript_table2 = transcript_table.drop(columns=['exonNumber'])
     exonNumber.reverse()
@@ -37,36 +37,51 @@ g2t = pd.read_csv('g2t_210907_b38.tsv', sep='\t')
 
 mane_transcripts = list(g2t.iloc[:,1])
 len(mane_transcripts)
-for transcript in range(10):
-    print(mane_transcripts[transcript])
+# mane_transcripts_small = mane_transcripts[:10]
+
+txs_notin_cellbase = 0
+#query data from a small list(5) of transcript rather than the range
+for transcript in mane_transcripts:
+    # get transcript from cellbase via webserver
+    print(transcript)
     url_get = "/webservices/rest/v5/hsapiens/feature/transcript/"
-    txs = mane_transcripts[transcript]
+    txs = transcript
     url_end = "/info?source=refseq"
     url_address = host_address + url_get + txs + url_end
     with urllib.request.urlopen(url_address) as url:
         data = json.loads(url.read().decode())
     
-    #number of exons: 
-    exons_num = len(data['responses'][0]['results'][0]['exons'])
-    
-    list_txs_dict = []
-    for exon in range(exons_num):
-        # print(exon)
-        exon_dict = data['responses'][0]['results'][0]['exons'][exon]
-        #print(exon_dict)
-        if exon_dict["phase"] != -1:
-            extracted_exons_dict = query_cellbasedict(exon_dict)
-            list_txs_dict.append(extracted_exons_dict)
-            # print(list_txs_dict)
+    if not data['responses'][0]['results']:
+        print("RefSeq transcript does not exist in cellbase")
+        txs_notin_cellbase += 1 
+    else:
+        #number of exons: 
+        exons_num = len(data['responses'][0]['results'][0]['exons'])
+        
+        # loop through each exon for a transcript and make a long list of dict
+        list_txs_dict = []
+        for exon in range(exons_num):
+            # print(exon)
+            exon_dict = data['responses'][0]['results'][0]['exons'][exon]
+            #print(exon_dict)
+            if exon_dict["phase"] != -1:
+                extracted_exons_dict = query_cellbasedict(exon_dict)
+                list_txs_dict.append(extracted_exons_dict)
+                # print(list_txs_dict)
+            else:
+                print("exon " + str(exon) + " is a non coding exon")
+        
+        # combine all exons into one table for that transcript
+        transcript_table = pd.DataFrame(list_txs_dict)
+        
+        #flip exon numbers if transcript is on -ve strand
+        if data['responses'][0]['results'][0]['strand'] == "-":
+            transcript_table2 = flip_exonNumber(transcript_table)
         else:
-            print("exon " + str(exon) + " is a non coding exon")
-    
-    # combine all exons into one table for that transcript
-    transcript_table = pd.DataFrame(list_txs_dict)
-    
-    if data['responses'][0]['results'][0]['strand'] == "-":
-        transcript_table2 = flip_exonNumber(transcript_table)
-    
-    df = df.append(transcript_table2) 
+            # since table2 is binded, we can't forget about the positive strands
+            transcript_table2 = transcript_table
+        
+        #append to main df of all transcripts
+        df = df.append(transcript_table2) 
 
-df.to_csv("exon_test.tsv", sep="\t", header=True, index=False)
+df.to_csv("exons_210915_b38.tsv", sep="\t", header=True, index=False)
